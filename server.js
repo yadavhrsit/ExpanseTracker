4666198748e764e6b6196d195d761584c48d06de
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-07-01 12:26:57 +0530

    Database Added

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = express();
+const Port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-const Port = 8000;
-
 app.listen(Port, () => {
     console.log(`Server Started on Port ${Port}`);
-})
+});
+
+mongoose.connect(process.env.DB)
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((error) => {
+        console.log("DB Connection Failed:", error);
+    });
